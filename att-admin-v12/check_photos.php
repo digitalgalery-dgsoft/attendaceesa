63685d48fc829f6cb0a0fcb294cee84b576c73ff
Jeigray <abdurrahmanjamil.mail@gmail.com>
2026-08-14 08:58:29 +0700

    chore: remove temp script

--- att-admin-v12/check_photos.php
+++
@@ -1,7 +0,0 @@
-<?php
-require __DIR__.'/vendor/autoload.php';
-$app = require_once __DIR__.'/bootstrap/app.php';
-$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
-
-$reports = \App\Models\VisitReport::latest()->take(5)->get(['id', 'photo_path'])->toArray();
-print_r($reports);
