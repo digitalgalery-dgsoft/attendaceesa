40a530560a979b1ae972ffd4b6e3c24d42a14402
Jeigray <abdurrahmanjamil.mail@gmail.com>
2026-08-24 10:18:39 +0700

    fix(filament): Perbaiki namespace import Section dan Grid ke Filament\Schemas\Components

--- att-admin-v12/app/Filament/Resources/Principals/Schemas/PrincipalForm.php
+++ att-admin-v12/app/Filament/Resources/Principals/Schemas/PrincipalForm.php
@@ -2,15 +2,14 @@
 
 namespace App\Filament\Resources\Principals\Schemas;
 
-use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\ColorPicker;
-use Filament\Forms\Components\Grid;
-use Filament\Forms\Components\Section;
+use Filament\Forms\Components\FileUpload;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
-use Filament\Forms\Components\Placeholder;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
 
--- att-admin-v12/app/Filament/Resources/ReportTemplates/Schemas/ReportTemplateForm.php
+++ att-admin-v12/app/Filament/Resources/ReportTemplates/Schemas/ReportTemplateForm.php
@@ -2,15 +2,15 @@
 
 namespace App\Filament\Resources\ReportTemplates\Schemas;
 
-use Filament\Forms\Components\Grid;
 use Filament\Forms\Components\KeyValue;
 use Filament\Forms\Components\Repeater;
-use Filament\Forms\Components\Section;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\TagsInput;
 use Filament\Forms\Components\TextInput;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\Toggle;
+use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
 use Illuminate\Support\Str;
 
